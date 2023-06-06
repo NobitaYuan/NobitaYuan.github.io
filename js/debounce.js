d3c59795e40ae2940e6a99ab6bfeaaffd273cf6b
NobitaYuan <1723936262@qq.com>
2023-06-06 22:36:16 +0800

    Site updated: 2023-06-06 22:36:15

---
+++ js/debounce.js
@@ -0,0 +1,26 @@
+// 防抖全局计时器
+let TT = null;    //time用来控制事件的触发
+// 防抖函数:fn->逻辑 time->防抖时间
+function debounce(fn, time) {
+    if (TT !== null) clearTimeout(TT);
+    TT = setTimeout(fn, time);
+}
+
+// 复制提醒
+document.addEventListener("copy", function () {
+    debounce(function () {
+        new Vue({
+            data: function () {
+                this.$notify({
+                    title: "复制成功!",
+                    message: "若要转载,请保留原文链接哦!！",
+                    position: 'top-left',
+                    offset: 50,
+                    showClose: true,
+                    type: "success",
+                    duration: 5000
+                });
+            }
+        })
+    }, 300);
+})
